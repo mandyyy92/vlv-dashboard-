652e0b1a88af5fd6bc5d4ecbd21b9212ac2a7f3d
mandyyy92 <mandy@openhan.kr>
2026-05-28 00:21:40 +0900

    feat: 오더 입고현황 메뉴에 새 생산 관리 대시보드 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import ProductionDashboard from './ProductionDashboard.jsx'
-
-const isProductionPage = typeof window !== 'undefined' && window.location.search.includes('page=production')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {isProductionPage ? <ProductionDashboard /> : <App />}
+    <App />
   </React.StrictMode>,
 )
