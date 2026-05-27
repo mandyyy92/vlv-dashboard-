af93cdf8befbe98e697aff91ea10f60384423026
mandyyy92 <mandy@openhan.kr>
2026-05-28 00:07:09 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import ProductionDashboard from './ProductionDashboard.jsx'
 
-const isProductionPage = typeof window !== 'undefined' && window.location.search.includes('page=production')
+const isOldPage = typeof window !== 'undefined' && window.location.search.includes('page=old')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {isProductionPage ? <ProductionDashboard /> : <App />}
+    {isOldPage ? <App /> : <ProductionDashboard />}
   </React.StrictMode>,
 )
